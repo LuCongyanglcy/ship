a146f1ddd57ff772cbfaaceaccff5b4e61158b5e
lcy <2911630756@qq.com>
2020-05-18 18:43:08 +0800

    1

--- src/jiaoxi/crazyit/classes/Caculate.java
+++ src/jiaoxi/crazyit/classes/Caculate.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Caculate  implements Serializable{
 	/**
-	 * 
+	 * 1111
 	 */
 	private static final long serialVersionUID = -7215051415008682992L;
 	public double []seekData;
